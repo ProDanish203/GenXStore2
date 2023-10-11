73bbd7ad2c4325993bf8bca2ff5370b65de46b20
Danish <danishsidd524@gmail.com>
2023-10-11 17:22:48 +0500

    Bug fix

--- app/api/product/getProducts/route.js
+++ app/api/product/getProducts/route.js
@@ -1,11 +1,10 @@
 import Product from "@../../../lib/models/Product";
 import {connectDb} from "@../../../lib/config/db";
 
+export const revalidate = 1;
 export const GET = async (req) => {
     try{
         connectDb();
-        // const cat = await req.nextUrl.searchParams.get('cat');
-
         const product = await Product.find();
 
         if(!product) return new Response(`Failed to fetch products`, {status: 500});
